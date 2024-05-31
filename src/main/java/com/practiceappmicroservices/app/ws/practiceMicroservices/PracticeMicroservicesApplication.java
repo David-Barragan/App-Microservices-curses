7eb61cff15c8207796067228d7e9cb9152a18405
cristian <cristian.c.lopez.m@gmail.com>
2024-05-31 11:04:01 -0500

    practice microservices v1

---
+++ src/main/java/com/practiceappmicroservices/app/ws/practiceMicroservices/PracticeMicroservicesApplication.java
@@ -0,0 +1,14 @@
+package com.practiceappmicroservices.app.ws.practiceMicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeMicroservicesApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/practiceappmicroservices/app/ws/practiceMicroservices/PracticeMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practiceappmicroservices.app.ws.practiceMicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
